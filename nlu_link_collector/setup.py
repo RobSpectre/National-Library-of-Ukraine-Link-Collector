4dd002c503d6f3b10d4b46267d4151ce5f7d3a86
Rob Spectre <rob@brooklynhacker.com>
2022-03-03 13:51:27 -0500

    Move resources file.

--- nlu_link_collector/setup.py
+++ nlu_link_collector/setup.py
@@ -7,7 +7,7 @@ setup(
     version      = '1.0',
     packages     = find_packages(),
     entry_points = {'scrapy': ['settings = nlu_link_collector.settings']},
-    package_data= {
+    package_data = {
         'nlu_link_collector': ['resources/*.csv']
     },
     zip_safe=False
