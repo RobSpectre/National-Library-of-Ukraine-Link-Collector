f055acfb5753c2eb6b4896a8171996829a9b462f
Rob Spectre <rob@brooklynhacker.com>
2022-03-03 13:24:27 -0500

    1) First draft of link collector.

---
+++ nlu_link_collector/nlu_link_collector/spiders/nlu_link_collector.py
@@ -0,0 +1,44 @@
+import csv
+
+from scrapy.spiders import CrawlSpider, Rule, Request
+from scrapy.linkextractors import LinkExtractor
+from scrapy.selector import Selector
+
+from nlu_link_collector.items import NluLinkCollectorItem
+
+
+class NluLinkCollector(CrawlSpider):
+    name = "nlu_link_collector"
+
+    def start_requests(self):
+        urls = []
+
+        with open('nlu_links.csv') as csv_file:
+            data = csv.reader(csv_file)
+            next(data, None)
+            for row in data:
+                urls.append(row[0])
+
+        self.logger.warn(urls[0])
+
+        for url in urls:
+            yield Request(url, self.parse_items)
+
+    def parse_items(self, response):
+        hxs = Selector(response)
+
+        xpath = "//b[text()=' НАДХОДЖЕННЯ:']/following::table[3]/tr"
+
+        for row in hxs.xpath(xpath):
+            year = row.xpath(".//b[1]/text()").extract()
+
+            for link in row.xpath('.//a'):
+                item = NluLinkCollectorItem()
+
+                item['year'] = year
+                item['volume'] = link.xpath("./text()").extract()
+                item['uri'] = link.xpath("./@href").extract()
+                item['source_uri'] = response.url
+
+                if item['uri']:
+                    yield item
